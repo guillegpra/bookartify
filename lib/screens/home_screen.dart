ca327239cddf8a939d810111a30bdc8c99f3270d
Guillermo García <garcag@ITSERV.SCSS.TCD.IE>
2023-06-29 16:09:13 +0100

    Home screen (by Delia)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,19 +3,63 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SearchScanBar(),
-      body: Center(
-        child: Text(
-          "Home Screen",
-          style: GoogleFonts.poppins(
-              fontSize: 24
+      appBar: const SearchScanBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(245, 239, 225, 1),
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                  color: const Color.fromRGBO(191, 160, 84, 1),
+                  width: 2.0,
+                ),
+              ),
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "For You",
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  color: const Color.fromRGBO(47, 47, 47, 1),
+                ),
+              ),
+            ),
           ),
-        ),
+          Card(
+            color: const Color.fromRGBO(245, 239, 225, 1),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'images/forYouSample.jpg'
+                ), // Replace with your desired image asset path
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Art Work Title',
+                    style: GoogleFonts.poppins(fontSize: 18),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'By artist name',
+                    style:
+                    GoogleFonts.poppins(fontSize: 14, color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
