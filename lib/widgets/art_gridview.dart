76d5c8e82b26e17b4f239ee6b08334c762006e27
shraynamboori <shraykumarpanther@gmail.com>
2023-07-03 15:21:32 +0100

    widget to arrange pictures into grids

---
+++ lib/widgets/art_gridview.dart
@@ -0,0 +1,25 @@
+import 'package:bookartify/data/dummy_art_data.dart';
+import 'package:bookartify/widgets/art_image_container.dart';
+import 'package:flutter/material.dart';
+
+class ArtGridView extends StatelessWidget {
+  const ArtGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(12,16,12,12),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2 / 3,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 18,
+        ),
+        itemCount: artworksForThatBook.length,
+        itemBuilder: (ctx, i) =>
+            ArtImageContainer(artwork: artworksForThatBook[i]),
+      ),
+    );
+  }
+}
