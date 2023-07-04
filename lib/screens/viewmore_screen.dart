d6952c15129f221bedde38d468e0d3a5dcfd396c
Delia Lynch <lynchd20@ITSERV.SCSS.TCD.IE>
2023-07-04 18:59:35 +0100

    view more page edits

---
+++ lib/screens/viewmore_screen.dart
@@ -0,0 +1,112 @@
+import 'package:bookartify/widgets/search_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ViewMoreScreen extends StatelessWidget {
+  const ViewMoreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const SearchScanBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(245, 239, 225, 1),
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                  color: const Color.fromRGBO(191, 160, 84, 1),
+                  width: 2.0,
+                ),
+              ),
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Genre",
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  color: const Color.fromRGBO(47, 47, 47, 1),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(
+                        bottom: 16.0), // Add margin between cards
+                    child: Card(
+                      color: const Color.fromRGBO(245, 239, 225, 1),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            'images/forYouSample.jpg',
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    'Art Work Title',
+                                    style: GoogleFonts.dmSerifDisplay(
+                                        fontSize: 18),
+                                  ),
+                                ),
+                                const SizedBox(width: 8.0),
+                                const Icon(
+                                  Icons.favorite_outline,
+                                  color: Color.fromRGBO(55, 34, 19, 1),
+                                  size: 24.0,
+                                  semanticLabel:
+                                      'Text to announce in accessibility modes',
+                                ),
+                                const SizedBox(width: 8.0),
+                                const Icon(
+                                  Icons.bookmark_outline,
+                                  color: Color.fromRGBO(55, 34, 19, 1),
+                                  size: 24.0,
+                                  semanticLabel:
+                                      'Text to announce in accessibility modes',
+                                ),
+                                const SizedBox(width: 8.0),
+                                const Icon(
+                                  Icons.share_outlined,
+                                  color: Color.fromRGBO(55, 34, 19, 1),
+                                  size: 24.0,
+                                  semanticLabel:
+                                      'Text to announce in accessibility modes',
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'By artist name',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 14, color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
