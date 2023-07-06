b098a609f0de9b6ec6f9df7baee2d38ff8434f54
shraynamboori <shraykumarpanther@gmail.com>
2023-07-06 14:39:37 +0100

    re-implemented search bar widget

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -81,3 +81,56 @@ class _SearchScanBarState extends State<SearchScanBar> {
     super.dispose();
   }
 }
+
+//OLD SEARCHBAR CODE  
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// class SearchScanBar extends StatelessWidget implements PreferredSizeWidget {
+//   const SearchScanBar({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       backgroundColor: Colors.transparent,
+//       elevation: 0,
+//       title: Container(
+//         decoration: BoxDecoration(
+//             color: const Color(0xFFE3D4B5),
+//             borderRadius: BorderRadius.circular(15)
+//         ),
+//         child: TextField(
+//           style: GoogleFonts.poppins(
+//               color: const Color(0xFF372213)
+//           ),
+//           decoration: const InputDecoration(
+//               hintText: "Search by title/author or ISBN",
+//               prefixIcon: Icon(Icons.search),
+//               border: InputBorder.none
+//           ),
+//           onChanged: (value) {
+//             // Handle search input changes
+//           },
+//         ),
+//       ),
+//       centerTitle: true,
+//       actions: [
+//         IconButton(
+//             onPressed: () {
+//               // Handle scan icon press
+//             },
+//             icon: const Icon(
+//               Icons.qr_code_scanner,
+//               color: Color(0xFF372213),
+//             )
+//         )
+//       ],
+//     );
+//   }
+
+//   @override
+//   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+// }
+
+
+
