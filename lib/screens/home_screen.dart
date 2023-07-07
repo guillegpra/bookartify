d76b7183542fba1c9c8b0d699cf22b14df197e61
shraynamboori <shraykumarpanther@gmail.com>
2023-07-07 11:05:04 +0100

    imported search screen and inactive search bar widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:bookartify/widgets/save_icon.dart';
-import 'package:bookartify/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:bookartify/widgets/share_button.dart';
 import 'package:bookartify/widgets/like_icon.dart';
 import 'package:bookartify/widgets/save_icon.dart';
+import 'package:bookartify/screens/search_screen.dart'; 
+import 'package:bookartify/widgets/inactive_searchbar.dart'; 
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: const SearchScanBar(),
+      appBar: InactiveSearchBar(),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -44,8 +45,7 @@ class HomeScreen extends StatelessWidget {
                 itemCount: 3,
                 itemBuilder: (context, index) {
                   return Container(
-                    margin: const EdgeInsets.only(
-                        bottom: 16.0), // Add margin between cards
+                    margin: const EdgeInsets.only(bottom: 16.0), // Add margin between cards
                     child: Card(
                       color: const Color.fromRGBO(245, 239, 225, 1),
                       shape: RoundedRectangleBorder(
@@ -71,8 +71,7 @@ class HomeScreen extends StatelessWidget {
                                 Expanded(
                                   child: Text(
                                     'Art Work Title',
-                                    style: GoogleFonts.dmSerifDisplay(
-                                        fontSize: 18),
+                                    style: GoogleFonts.dmSerifDisplay(fontSize: 18),
                                   ),
                                 ),
                                 SizedBox(width: 8.0),
@@ -90,8 +89,7 @@ class HomeScreen extends StatelessWidget {
                             padding: const EdgeInsets.all(8.0),
                             child: Text(
                               'By artist name',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14, color: Colors.grey),
+                              style: GoogleFonts.poppins(fontSize: 14, color: Colors.grey),
                             ),
                           ),
                         ],
@@ -107,3 +105,123 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+//old code before searchbar implementation
+// import 'package:bookartify/widgets/save_icon.dart';
+// import 'package:bookartify/widgets/search_bar.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:bookartify/widgets/share_button.dart';
+// import 'package:bookartify/widgets/like_icon.dart';
+// import 'package:bookartify/widgets/save_icon.dart';
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       // appBar: const SearchScanBar(),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Container(
+//               decoration: BoxDecoration(
+//                 color: const Color.fromRGBO(245, 239, 225, 1),
+//                 borderRadius: BorderRadius.circular(8.0),
+//                 border: Border.all(
+//                   color: const Color.fromRGBO(191, 160, 84, 1),
+//                   width: 2.0,
+//                 ),
+//               ),
+//               padding: const EdgeInsets.all(8.0),
+//               child: Text(
+//                 "For You",
+//                 style: GoogleFonts.poppins(
+//                   fontSize: 18,
+//                   color: const Color.fromRGBO(47, 47, 47, 1),
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Flexible(
+//             child: Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 10.0),
+//               child: ListView.builder(
+//                 itemCount: 3,
+//                 itemBuilder: (context, index) {
+//                   return Container(
+//                     margin: const EdgeInsets.only(
+//                         bottom: 16.0), // Add margin between cards
+//                     child: Card(
+//                       color: const Color.fromRGBO(245, 239, 225, 1),
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Padding(
+//                             padding: const EdgeInsets.all(8.0),
+//                             child: ClipRRect(
+//                               borderRadius: BorderRadius.circular(10),
+//                               child: Image.asset(
+//                                 'images/forYouSample.jpg',
+//                               ),
+//                             ),
+//                           ),
+//                           Padding(
+//                             padding: const EdgeInsets.all(8.0),
+//                             child: Row(
+//                               mainAxisAlignment: MainAxisAlignment.end,
+//                               children: [
+//                                 Expanded(
+//                                   child: Text(
+//                                     'Art Work Title',
+//                                     style: GoogleFonts.dmSerifDisplay(
+//                                         fontSize: 18),
+//                                   ),
+//                                 ),
+//                                 SizedBox(width: 8.0),
+//                                 LikeIcon(),
+//                                 SizedBox(width: 8.0),
+//                                 SaveIcon(),
+//                                 SizedBox(width: 8.0),
+//                                 ShareButton(onPressed: () {
+//                                   // TODO: share functionality
+//                                 }),
+//                               ],
+//                             ),
+//                           ),
+//                           Padding(
+//                             padding: const EdgeInsets.all(8.0),
+//                             child: Text(
+//                               'By artist name',
+//                               style: GoogleFonts.poppins(
+//                                   fontSize: 14, color: Colors.grey),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   );
+//                 },
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
