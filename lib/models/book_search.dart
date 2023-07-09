da6b4366f434efdc47d1675ad2cfc9de28ca8093
shraynamboori <shraykumarpanther@gmail.com>
2023-07-09 23:04:44 +0100

    added more parameters

--- lib/models/book_search.dart
+++ lib/models/book_search.dart
@@ -1,26 +1,33 @@
 class Book {
   final String title;
   final String author;
-  final String description;
   final String thumbnailUrl;
+  final String publishedDate;
+  final int pageCount;
+  final String description; // added
 
-  Book({
-    required this.title,
-    required this.author,
-    required this.description,
-    required this.thumbnailUrl,
-  });
+  Book(
+      {required this.title,
+      required this.author,
+      required this.thumbnailUrl,
+      required this.publishedDate,
+      required this.pageCount,
+      required this.description});
 
   factory Book.fromJson(Map<String, dynamic> json) {
     return Book(
-      title: json['volumeInfo']['title'] ?? 'Unknown Title',
-      author: (json['volumeInfo']['authors'] != null)
-          ? json['volumeInfo']['authors'].join(', ')
-          : 'Unknown Author',
-      description: json['volumeInfo']['description'] ?? 'No description available',
-      thumbnailUrl: json['volumeInfo']['imageLinks'] != null && json['volumeInfo']['imageLinks']['smallThumbnail'] != null
-          ? json['volumeInfo']['imageLinks']['smallThumbnail']
-          : 'images/search_placeholder_image.jpg', //add a placeholder image URL here
+      title: json['volumeInfo']['title'] ?? "Title not available",
+      author: (json['volumeInfo']['authors'] != null &&
+              json['volumeInfo']['authors'].isNotEmpty)
+          ? json['volumeInfo']['authors'][0]
+          : "Author not available",
+      thumbnailUrl: json['volumeInfo']['imageLinks'] != null
+          ? json['volumeInfo']['imageLinks']['thumbnail']
+          : "images/search_placeholder_image.jpg",
+      publishedDate: json['volumeInfo']['publishedDate'] ?? "No release date",
+      pageCount: json['volumeInfo']['pageCount'] ?? 0,
+      description:
+          json['volumeInfo']['description'] ?? "No synopsis available", // added
     );
   }
 }
