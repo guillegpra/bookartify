7e28d1e1434b5160e3d033bc789fa72370d140ce
shraynamboori <shraykumarpanther@gmail.com>
2023-07-11 08:35:58 +0100

    adapted to be used inside singleChildScrollView

--- lib/widgets/art_gridview.dart
+++ lib/widgets/art_gridview.dart
@@ -1,14 +1,13 @@
 import 'package:bookartify/data/dummy_art_data.dart';
 import 'package:bookartify/widgets/art_image_container.dart';
 import 'package:flutter/material.dart';
-
 class ArtGridView extends StatelessWidget {
   const ArtGridView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(12,16,12,12),
+      padding: const EdgeInsets.fromLTRB(12,12,12,12),
       child: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -19,7 +18,36 @@ class ArtGridView extends StatelessWidget {
         itemCount: artworksForThatBook.length,
         itemBuilder: (ctx, i) =>
             ArtImageContainer(artwork: artworksForThatBook[i]),
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
       ),
     );
   }
 }
+
+
+// import 'package:bookartify/data/dummy_art_data.dart';
+// import 'package:bookartify/widgets/art_image_container.dart';
+// import 'package:flutter/material.dart';
+
+// class ArtGridView extends StatelessWidget {
+//   const ArtGridView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.fromLTRB(12,16,12,12),
+//       child: GridView.builder(
+//         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//           crossAxisCount: 2,
+//           childAspectRatio: 2 / 3,
+//           crossAxisSpacing: 12,
+//           mainAxisSpacing: 18,
+//         ),
+//         itemCount: artworksForThatBook.length,
+//         itemBuilder: (ctx, i) =>
+//             ArtImageContainer(artwork: artworksForThatBook[i]),
+//       ),
+//     );
+//   }
+// }
