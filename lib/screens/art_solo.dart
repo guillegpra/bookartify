c0f0b5e25de3671f71dd81d01aeea69fd262b316
guillermogarciapradales <guillegpra@gmail.com>
2023-07-25 21:18:15 +0100

    style changes

--- lib/screens/art_solo.dart
+++ lib/screens/art_solo.dart
@@ -18,7 +18,7 @@ class ArtSoloScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.grey[50],
         elevation: 0,
         centerTitle: true,
         leading: IconButton(
@@ -30,162 +30,169 @@ class ArtSoloScreen extends StatelessWidget {
         ),
         title: Text(
           imageTitle, // Display the image title in the app bar
-          style: GoogleFonts.poppins(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
+          style: GoogleFonts.dmSerifDisplay(
+            // fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  imagePath, // Display the clicked image
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.network(
+                    imagePath, // Display the clicked image
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(42),
-                  color: const Color.fromARGB(70, 192, 162, 73),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(42),
+                    color: const Color.fromARGB(70, 192, 162, 73),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(25, 8, 25, 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                imageTitle, // Display the image title
+                                style: GoogleFonts.dmSerifDisplay(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.w600
+                                ),
+                              ),
+                              Text(
+                                'By artist', // Display the artist name
+                                style: GoogleFonts.poppins(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w300
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              const LikeIcon(),
+                              const SizedBox(width: 10),
+                              const SaveIcon(),
+                              // const SizedBox(width: 20),
+                              ShareButton(
+                                onPressed: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                width: double.infinity,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(25, 8, 25, 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              imageTitle, // Display the image title
-                              style: GoogleFonts.poppins(
-                                  fontSize: 19, fontWeight: FontWeight.w500),
-                            ),
-                            Text(
-                              'By artist', // Display the artist name
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14, fontWeight: FontWeight.w300),
-                            ),
-                          ],
+                  padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
+                  child: Text(
+                    "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.....",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.grey),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        print("Button pressed!");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(
+                            255, 192, 162, 73), // background color
+                        foregroundColor: Colors.black, // text color
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(5), // button's corner radius
                         ),
                       ),
-                      Expanded(
-                        flex: 1,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            const LikeIcon(),
-                            const SizedBox(width: 10),
-                            const SaveIcon(),
-                            // const SizedBox(width: 20),
-                            ShareButton(
-                              onPressed: () {},
-                            ),
-                          ],
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 0,
+                        ),
+                        child: Text(
+                          "Add to Collection",
+                          style: GoogleFonts.poppins(
+                              fontSize: 16, fontWeight: FontWeight.w400),
                         ),
                       ),
-                    ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        print("Button pressed!");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 47, 47, 47),
+                        foregroundColor: Colors.white, // text color
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(5), // button's corner radius
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 28,
+                        ),
+                        child: Text(
+                          "View in AR",
+                          style: GoogleFonts.poppins(
+                              fontSize: 16, fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: Text(
+                    "View more works....",
+                    style: GoogleFonts.poppins(
+                        fontSize: 16, fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                child: Text(
-                  "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.....",
-                  style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.grey),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      print("Button pressed!");
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(
-                          255, 192, 162, 73), // background color
-                      foregroundColor: Colors.black, // text color
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(5), // button's corner radius
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 0,
-                      ),
-                      child: Text(
-                        "Add to Collection",
-                        style: GoogleFonts.poppins(
-                            fontSize: 16, fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("Button pressed!");
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 47, 47, 47),
-                      foregroundColor: Colors.white, // text color
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(5), // button's corner radius
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 28,
-                      ),
-                      child: Text(
-                        "View in AR",
-                        style: GoogleFonts.poppins(
-                            fontSize: 16, fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 18.0),
-                child: Text(
-                  "View more works....",
-                  style: GoogleFonts.poppins(
-                      fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            // Replace the ArtGridView() with the relevant widgets to show more works if needed
-          ],
+              const SizedBox(height: 10),
+              // Replace the ArtGridView() with the relevant widgets to show more works if needed
+            ],
+          ),
         ),
       ),
     );
