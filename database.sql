1eb0e91e4d7c11a99d570d071dd7bec77184b60c
Guillermo Garcia Pradales <guillegpra@gmail.com>
2023-07-26 14:57:56 +0100

    add description to posts

--- database.sql
+++ database.sql
@@ -47,6 +47,7 @@ CREATE TABLE follows_book (
 CREATE TABLE art (
     id int(11) NOT NULL AUTO_INCREMENT,
     title varchar(512) DEFAULT NULL,
+    description text DEFAULT NULL,
     book_id varchar(512) DEFAULT NULL, 
     user_id varchar(512) DEFAULT NULL,
     url varchar(2083) NOT NULL, -- image url
@@ -58,6 +59,7 @@ CREATE TABLE art (
 CREATE TABLE covers (
     id int(11) NOT NULL AUTO_INCREMENT,
     title varchar(512) DEFAULT NULL,
+    description text DEFAULT NULL,
     book_id varchar(512) DEFAULT NULL, 
     user_id varchar(512) DEFAULT NULL,
     url varchar(2083) NOT NULL, -- image url
@@ -121,59 +123,15 @@ INSERT INTO art (title, book_id, user_id, url) VALUES ("Marianne & Connell", "vr
 INSERT INTO bookmarks_cover (user_id, cover_id) VALUES ("S5zT8izdcGWIena7LDy4gvGD3xj1", 1);
 
 -- get bookmarks
-SELECT 'art' AS type, a.id, a.title, a.book_id, a.user_id, a.url, a.date_upload
+SELECT 'art' AS type, a.id, a.title, a.description, a.book_id, a.user_id, a.url, a.date_upload
 FROM art a
 JOIN bookmarks_art ba ON a.id = ba.art_id
 WHERE ba.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
 UNION
-SELECT 'cover' AS type, c.id, c.title, c.book_id, c.user_id, c.url, c.date_upload
+SELECT 'cover' AS type, c.id, c.title, c.description, c.book_id, c.user_id, c.url, c.date_upload
 FROM covers c
 JOIN bookmarks_cover bc ON c.id = bc.cover_id
 WHERE bc.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1";
-
--- for you page
-SELECT *
-FROM (
-    SELECT art.id, art.title, art.book_id, art.user_id, art.url, art.date_upload
-    FROM art
-    INNER JOIN follows_user ON art.user_id = follows_user.following_id
-    WHERE follows_user.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
-    
-    UNION ALL
-    
-    SELECT covers.id, covers.title, covers.book_id, covers.user_id, covers.url, covers.date_upload
-    FROM covers
-    INNER JOIN follows_user ON covers.user_id = follows_user.following_id
-    WHERE follows_user.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
-    
-    UNION ALL
-    
-    SELECT art.id, art.title, art.book_id, art.user_id, art.url, art.date_upload
-    FROM art
-    WHERE art.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
-    
-    UNION ALL
-    
-    SELECT covers.id, covers.title, covers.book_id, covers.user_id, covers.url, covers.date_upload
-    FROM covers
-    WHERE covers.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
-    
-    UNION ALL
-    
-    SELECT art.id, art.title, art.book_id, art.user_id, art.url, art.date_upload
-    FROM art
-    INNER JOIN follows_book ON art.book_id = follows_book.following_id
-    WHERE follows_book.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
-    
-    UNION ALL
-    
-    SELECT covers.id, covers.title, covers.book_id, covers.user_id, covers.url, covers.date_upload
-    FROM covers
-    INNER JOIN follows_book ON covers.book_id = follows_book.following_id
-    WHERE follows_book.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
-) AS all_posts
-GROUP BY id
-ORDER BY date_upload DESC;
 
 -- get following
 SELECT GROUP_CONCAT(DISTINCT following_id) as following FROM follows_user WHERE user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"; -- list
@@ -187,17 +145,46 @@ SELECT COUNT(DISTINCT user_id) as followers FROM follows_user WHERE following_id
 SELECT user_id, GROUP_CONCAT(DISTINCT follower_id SEPARATOR ', ') as following 
 FROM follows_user GROUP BY user_id;
 
-/*
-DELETE FROM photos WHERE id = 2;
-DELETE FROM comments WHERE id = 36;
-
-SELECT photos.id, photos.caption, photos.alt_text, users.username, photos.photo_path, photos.date_upload
-FROM photos, users
-WHERE photos.user_id = users.id
-ORDER BY photos.date_upload DESC;
-
-SELECT photos.id, photos.caption, photos.alt_text, users.username, photos.photo_path, photos.date_upload, 
-	(SELECT comments.content FROM comments WHERE comments.photo_id = photos.id) AS photo_comment_content
-FROM photos, users
-WHERE photos.user_id = users.id
-ORDER BY photos.date_upload DESC; */
+-- for you page
+SELECT type, id, title, description, book_id, user_id, url, date_upload
+FROM (
+    SELECT 'art' AS type, a.id, a.title, a.description, a.book_id, a.user_id, a.url, a.date_upload
+    FROM art a
+    INNER JOIN follows_user ON a.user_id = follows_user.following_id
+    WHERE follows_user.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
+    
+    UNION ALL
+    
+    SELECT 'cover' AS type, c.id, c.title, c.description, c.book_id, c.user_id, c.url, c.date_upload
+    FROM covers c
+    INNER JOIN follows_user ON c.user_id = follows_user.following_id
+    WHERE follows_user.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
+    
+    UNION ALL
+    
+    SELECT 'art' AS type, a.id, a.title, a.description, a.book_id, a.user_id, a.url, a.date_upload
+    FROM art a
+    WHERE a.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
+    
+    UNION ALL
+    
+    SELECT 'cover' AS type, c.id, c.title, c.description, c.book_id, c.user_id, c.url, c.date_upload
+    FROM covers c
+    WHERE c.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
+    
+    UNION ALL
+    
+    SELECT 'art' AS type, a.id, a.title, a.description, a.book_id, a.user_id, a.url, a.date_upload
+    FROM art a
+    INNER JOIN follows_book ON a.book_id = follows_book.following_id
+    WHERE follows_book.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
+    
+    UNION ALL
+    
+    SELECT 'cover' AS type, c.id, c.title, c.description, c.book_id, c.user_id, c.url, c.date_upload
+    FROM covers c
+    INNER JOIN follows_book ON c.book_id = follows_book.following_id
+    WHERE follows_book.user_id = "S5zT8izdcGWIena7LDy4gvGD3xj1"
+) AS all_posts
+GROUP BY type, id, title, description, book_id, user_id, url, date_upload
+ORDER BY date_upload DESC;
