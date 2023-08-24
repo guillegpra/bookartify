1a8bf2f23e1384714c446460d5c3050a518d220f
guillermogarciapradales <guillegpra@gmail.com>
2023-08-24 16:28:54 +0200

    remove unused files

--- lib/models/variety_art.dart
+++
@@ -1,7 +0,0 @@
-class VarietyArt {
-  const VarietyArt({required this.artTitle, required this.artistName, required this.artImagePath});
-
-  final String artTitle;
-  final String artistName;
-  final String artImagePath;
-}
